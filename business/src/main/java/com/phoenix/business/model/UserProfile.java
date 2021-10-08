be2a79b9f294e64322cb35ce52f04dabffd617bf
dangdinhtai0001 <dangdinhtai_t61@hus.edu.vn>
2021-10-08 14:40:38 +0700

    re- config module business

---
+++ business/src/main/java/com/phoenix/business/model/UserProfile.java
@@ -0,0 +1,26 @@
+package com.phoenix.business.model;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UserProfile{
+    private Long id;
+    private String username;
+    private String status;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String mobileNumber;
+    private String address;
+    private Date hiredDate;
+
+    private String title;
+    private String department;
+}
