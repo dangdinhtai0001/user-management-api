40fc9a3f15ba75b6f309e6b4d31ec67e00160c97
dangdinhtai0001@gmail.com <dangdinhtai0001>
2021-10-31 01:25:23 +0700

    update default exception code

--- core/src/main/java/com/phoenix/core/config/DefaultExceptionCode.java
+++ core/src/main/java/com/phoenix/core/config/DefaultExceptionCode.java
@@ -1,15 +1,20 @@
 package com.phoenix.core.config;
 
 public class DefaultExceptionCode {
-    public static final String BAD_CREDENTIALS = "AUTH_001";
-    public static final String ACCOUNT_LOCKED = "AUTH_002";
-    public static final String ACCOUNT_EXPIRE = "AUTH_003";
-    public static final String ACCESS_DENIED = "AUTH_004";
-    public static final String DATABASE_ERROR = "DB_001";
-    public static final String INVALID_REFRESH_JWT = "AUTH_005";
-    public static final String BAD_REQUEST = "COM_001";
-    public static final String INTERNAL_ERROR = "COM_002";
-    public static final String NOT_FOUND = "COM_003";
-    public static final String METHOD_NOT_ALLOWED = "COM_004";
-    public static final String NOT_ACCEPTABLE = "COM_005";
+    /**
+     * 000: lỗi không thuộc service nào || Lỗi xuất hiện trên nhiều service
+     * 001: Lỗi của auth service
+     * 002: Lỗi của user service
+     */
+    public static final String BAD_CREDENTIALS = "001001";
+    public static final String ACCOUNT_LOCKED = "001002";
+    public static final String ACCOUNT_EXPIRE = "001003";
+    public static final String ACCESS_DENIED = "001004";
+    public static final String INVALID_REFRESH_JWT = "001005";
+    public static final String BAD_REQUEST = "000001";
+    public static final String INTERNAL_ERROR = "000002";
+    public static final String NOT_FOUND = "000003";
+    public static final String METHOD_NOT_ALLOWED = "000004";
+    public static final String NOT_ACCEPTABLE = "000005";
+    public static final String DATABASE_ERROR = "000006";
 }
